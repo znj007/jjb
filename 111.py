d1c40af4075cbd9239bd433032e9a28d2090ff4b
znj007 <85116830+znj007@users.noreply.github.com>
2021-06-07 14:58:36 +0900

    Update 111.py

--- 111.py
+++ 111.py
@@ -43,26 +43,28 @@ while True:
     
 
     
-        ma4 = df.rolling(window=2).mean()
-        ma15 = df.rolling(window=4).mean()
+        ma4 = df.rolling(window=1).mean()
+        ma15 = df.rolling(window=30).mean()
     
-        test1=ma4.iloc[-3]-ma15.iloc[-3]
-        test2=ma4.iloc[-2]-ma15.iloc[-2]
+        test1=ma4.iloc[-2]-ma15.iloc[-2]
+        test2=ma4.iloc[-1]-ma15.iloc[-1]
+        test3=ma15.iloc[-1]-ma15.iloc[-2]
     
         call='해당없음'
     
-        if test2<0:
+        if test2<0 and test3<0:
            call='판매' 
            eth = get_balance("ETH")
            if eth > 0.00008:
               upbit.sell_market_order("KRW-ETH", eth*)
+              time.sleep(1)
         
-        if test1<0 and test2>0:
+        if test1<0 and test2>0 and test3>0:
            call='구입'  
            krw = get_balance("KRW")
            if krw > 5000:
               upbit.buy_market_order("KRW-ETH", krw*0.9995)
-
+              times.sleep(1)
     
         time.sleep(1)
 
